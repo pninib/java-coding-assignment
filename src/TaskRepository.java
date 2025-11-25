4b84a7aacd03f1272ed31605870a9d4cdcd22144
pninib <p7624859.com>
2025-11-25 18:43:19 +0200

    Add TaskRepository: create class and methods

--- src/TaskRepository.java
+++ src/TaskRepository.java
@@ -1,3 +1,46 @@
-public class TaskRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TaskRepository {
+    private List<Task> tasks = new ArrayList<>();
+    private final String filePath = "src/tasks.json";
+
+    //public TaskRepository(){
+//    loadFromFile();
+//}
+    private void saveToFile() {
+
+    }
+
+    public void Add(Task task) {
+        tasks.add(task);
+        saveToFile();
+    }
+
+    public void Update(Task task) {
+        for (int i = 0; i < tasks.size(); i++) {
+            if (task.getId() == tasks.get(i).getId()) {
+                tasks.set(i, task);
+                break;
+            }
+        }
+        saveToFile();
+    }
+
+    public void Delete(int id) {
+        tasks.removeIf(t -> t.getId() == id);
+        saveToFile();
+    }
+
+    public Task GetById(int id) {
+        for (Task t : tasks) {
+            if (t.getId() == id)
+                return t;
+        }
+        return null;
+    }
+
+    public List<Task> AllList() {
+        return tasks;
+    }
 }
